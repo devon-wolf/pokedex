11e5b8846fe2ef21ad62d9b1abe37c1c40b99491
Devon Wolfkiel <devonwolf@tutanota.com>
2021-02-12 11:34:54 -0800

    Various styling changes

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import style from "../stylesheets/Common.module.css"
+import style from "../stylesheets/HomePage.module.css"
 
 export default class HomePage extends Component {
 	render() {
 		return (
 			<main className={style.main}>
-				<h1>WELCOME HOME</h1>
+				<h1>Click to search your pokedex</h1>
 			</main>
 		)
 	}
