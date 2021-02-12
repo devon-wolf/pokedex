5d03d9bdbcea06633727b0e286b9f1eb78f1ad15
Devon Wolfkiel <devonwolf@tutanota.com>
2021-02-12 13:29:42 -0800

    Update more styles, add link to search in body of homepage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 import style from "../stylesheets/HomePage.module.css"
 
 export default class HomePage extends Component {
 	render() {
 		return (
 			<main className={style.main}>
-				<h1>Click to search your pokedex</h1>
+				<NavLink exact className={style.link} to="/search"><h1>Click to search your pokedex</h1></NavLink>
 			</main>
 		)
 	}
