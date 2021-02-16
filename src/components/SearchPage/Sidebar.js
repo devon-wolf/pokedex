20da77a9a5ee0e87bd011121cc5a59411138f0b3
devon-wolf <33987744+devon-wolf@users.noreply.github.com>
2021-02-16 09:11:32 -0800

    Remove commented out code
    Remove old static radio buttons

--- src/components/SearchPage/Sidebar.js
+++ src/components/SearchPage/Sidebar.js
@@ -14,23 +14,6 @@ export default class Sidebar extends Component {
 				<SearchBar handleInput={this.props.handleSearch} placeholder='Search pokedex' />
 
 				<RadioButtonGroup handleRadio={this.props.handleRadio}/>
-
-				{/* <label className={style.radioHeader}>Search within type:</label>
-				<div className={style.radioSet}>
-					<RadioButton handleRadio={this.props.handleRadio} name="type" value="bug" label="Bug" />
-					<RadioButton handleRadio={this.props.handleRadio} name="type" value="fire" label="Fire" />
-					<RadioButton handleRadio={this.props.handleRadio} name="type" value="grass" label="Grass" />
-					<RadioButton handleRadio={this.props.handleRadio} name="type" value="normal" label="Normal" />
-					<RadioButton handleRadio={this.props.handleRadio} name="type" value="water" label="Water" />
-					<RadioButton handleRadio={this.props.handleRadio} name="type" value="psychic" label="Psychic" />
-					<RadioButton handleRadio={this.props.handleRadio} name="type" value="poison" label="Poison" />
-					<RadioButton handleRadio={this.props.handleRadio} name="type" value="electric" label="Electric" />
-					<RadioButton handleRadio={this.props.handleRadio} name="type" value="fairy" label="Fairy" />
-					<RadioButton handleRadio={this.props.handleRadio} name="type" value="rock" label="Rock" />
-					<RadioButton handleRadio={this.props.handleRadio} name="type" value="ground" label="Ground" />
-					<RadioButton handleRadio={this.props.handleRadio} name="type" value="fighting" label="Fighting" />
-					<RadioButton handleRadio={this.props.handleRadio} name="type" value="all" label="All" />
-				</div> */}
 				
 				<hr />
 				
